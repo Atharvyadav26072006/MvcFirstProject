16c581b461c903e481fdb84062cf8ebe617a69e9
ATHARV YADAV <atharvy728@gmail.com>
2026-06-13 15:32:30 +0530

    Add dto Class

---
+++ Desktop/ASPNET 8 MVC CURD/MVC WebApp/MVC WebApp/Dto/UserDto.cs
@@ -0,0 +1,15 @@
+﻿namespace MVC_WebApp.Dto
+{
+    public class UserDto
+    {
+
+        public int Id { get; set; }
+
+        public string UserName { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+
+        public string Password { get; set; } = null!;
+    }
+}
